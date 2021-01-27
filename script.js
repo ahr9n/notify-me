120d1a4762ee3107771e357f80d860604b3af599
Moustafa Mohamed <51851957+M0D4@users.noreply.github.com>
2021-01-27 19:14:02 +0200

    Make alert available for all vJudge tabs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var text;
+var text, commentsIds = [], hasOld = 0;
 var managers = ["M0D4", "MuhammadJ", "ahmed_magdy1", "Redhwan", "AIC", "Abdeltwab"];
 
 function getJsonText(){
@@ -58,7 +58,8 @@ function getJsonText(){
     request.onreadystatechange = function() {
       if (request.readyState == 4){
             var x = request.responseText;
-            check(x);
+            if(!hasOld) getOldComments(x);
+            else check(x);
       }
     };
     text = request.responseText;
@@ -70,6 +71,30 @@ function isAdmin(author){
 
 function check(text){
     if(window.navigator.onLine){
+        //console.log("Online.\n"); 
+        //console.log("text: " + text);
+        if(text.length == 0) return;
+        //alert(text);
+        var obj = JSON.parse(text);
+        //console.log(obj["id"]);
+        if(obj["id"] == 0) return; //no comments
+        //console.log("phase 2");
+        var posts = obj["posts"];
+        for(var i = 0; i < posts.length; i++){
+            //console.log(isAdmin(posts[i]["author"]));
+            if(!isAdmin(posts[i]["author"])) continue;
+            if(posts[i]["content"] == undefined) continue;
+            if(posts[i]["parentId"] != 0) continue;
+            if(commentsIds.indexOf(posts[i]["id"]) != -1) continue;
+            alert(posts[i]["content"]);
+            commentsIds.push(posts[i]["id"]);
+        }
+    }
+}
+
+function getOldComments(text){
+    hasOld = 1;
+    if(window.navigator.onLine){
         if(text.length == 0) return;
         var obj = JSON.parse(text);
         if(obj["id"] == 0) return; //no comments
@@ -78,12 +103,9 @@ function check(text){
             if(!isAdmin(posts[i]["author"])) continue;
             if(posts[i]["content"] == undefined) continue;
             if(posts[i]["parentId"] != 0) continue;
-            if(localStorage.getItem(posts[i]["id"]) != null) continue;
-            localStorage.setItem(posts[i]["id"], posts[i]["id"]);
-            alert(posts[i]["content"]);
+            if(commentsIds.indexOf(posts[i]["id"]) != -1) continue;
+            commentsIds.push(posts[i]["id"]);
         }
     }
 }
-
-
 var myVar = setInterval(getJsonText, 1000);
