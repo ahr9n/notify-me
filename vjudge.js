bc4b97f5f359c0f51708a050114f81c3ac7ecec1
Ahmad AbdulRahman <52632898+AHR9N@users.noreply.github.com>
2021-02-03 23:42:07 +0200

    Update vjudge.js

--- vjudge.js
+++ vjudge.js
@@ -4,10 +4,4 @@ window.browser = (function () {
     return window.msBrowser || window.browser || window.chrome;
 })();
 
-chrome.browserAction.onClicked.addListener(buttonClicked);
-
-function buttonClicked(tab){
-	console.log("button clicked!");
-}
-
 // console.log(window.location.href);
